fb728872dd831df989103c02e1794abecb6664ff
Zacera <113979331+Zacera@users.noreply.github.com>
2024-06-05 01:16:56 +0200

    Tailwind + part of design

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      'custom': '#7A9D8C',
+    },
+  },
+  plugins: [],
+}
+
