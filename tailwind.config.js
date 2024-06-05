5ee1f996dbe991004864f7c6a2d90a1667b5b9fc
Zacera <113979331+Zacera@users.noreply.github.com>
2024-06-05 13:23:12 +0200

    some-responzivity

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    backgroundImage: theme => ({
+      'hero-pattern': "linear-gradient(42.46deg, #ABA8BF 0%, #F5E9F3 38.5%)",
+    }),
     colors: {
       'custom': '#7A9D8C',
+
     },
   },
   plugins: [],
